598f5be034ac3d58872f754006ae7a6fa127cfb4
Ogundimine Aderonke Yetunde <107619226+Bussyslim@users.noreply.github.com>
2022-09-30 14:17:17 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-
-    b_score = max(a_dictionary.values(), default=None)
-    for k, v in a_dictionary.items():
-        if v == b_score:
-            return k
+    if(a_dictionary):
+        return(max(a_dictionary, key=a_dictionary.get))
