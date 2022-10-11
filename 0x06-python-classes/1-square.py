27241f7bd36419d7d503f4ffa2c6fa15a2035ec6
Ogundimine Aderonke Yetunde <107619226+Bussyslim@users.noreply.github.com>
2022-10-11 21:45:27 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Square Class
-A Square Class
+"""
+Class Square: Create a square class that contains a size
 """
 
 
 class Square:
-
+    """ Class that defines a square with size"""
     def __init__(self, size):
-        """__init__
-        The __init__ method initializes the size value
-        of the square.
-        Attributes:
-            size (int): The size of the square.
-        """
+        """ Initialize variables """
         self.__size = size
